8c206223f0c66d7d7a78437e5872eae27a0a1127
unknown <mf.vallejo@uniandes.edu.co>
2019-11-10 14:32:18 -0500

    Borracion de cosas inutiles

--- WMP/WMPApp/urls.py
+++ WMP/WMPApp/urls.py
@@ -5,8 +5,6 @@ from django.contrib.auth.views import LoginView
 urlpatterns = [
     path(r'', views.home),
     path(r'login/', LoginView.as_view(template_name='WMPApp/login.html'), name='login'),
-    path(r'gerente/', views.gerente),
-    path(r'empleado/', views.empleado),
     path(r'equipo/', views.equipo),
     path(r'valorar/', views.valorar),
     path(r'perfil/', views.perfil),
--- WMP/WMPApp/views.py
+++ WMP/WMPApp/views.py
@@ -2,25 +2,9 @@ from django.shortcuts import render, HttpResponse
 # Create your views here.
 
 def home(request):
-    numbers = [1,2,3,4,5]
     name = 'Miguel Muñoz'
-
-    args = {'myName': name, 'numbers': numbers}
+    args = {'myName': name,}
     return render(request, 'WMPApp/home.html', args)
-
-def gerente(request):
-    name = 'Miguel Muñoz'
-    cargo='Gerente'
-
-    args = {'myName': name,  'miCargo': cargo}
-    return render(request,'WMPApp/gerente.html',args)
-
-def empleado(request):
-    name = 'Manuel Vallejo'
-    cargo ='Code Developer'
-
-    args = {'myName': name,  'miCargo': cargo}
-    return render(request,'WMPApp/empleado.html',args)
 
 def equipo(request):
     args = {"nombre": ["Camilo Suarez", "Jonathan Triana", "Pedro Rodriguez"]}
@@ -30,7 +14,6 @@ def valorar(request):
     name = 'Camilo Suarez'
     args = {'myName': name}
     return render(request,'WMPApp/valoracion.html',args)
-
 
 def perfil(request):
     name = 'Nicolas Mesa'
